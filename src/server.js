473766b9fc8e0264b2fa4d7ef79a2566f052514d
rav <szucsi1104@gmail.com>
2019-09-25 18:15:21 +0200

    Add DDoS Protection

--- src/server.js
+++ src/server.js
@@ -2,60 +2,98 @@ const   http  = require('http'),
         fs    = require('fs');
 
 const PORT = 1104 || process.env.PORT;
-const server = http.createServer(router);
+const server = http.createServer(ddosProtection);
 
-// 0 - index.html | 1 - styles.css | 2 - script.js
+// 
 let files = [];
 
-const DOS_BLOCK_TIMEOUT = 0.5 * 60000, DOS_PROTECTION_TIMEOUT = 0.1 * 60000, DOS_PLUS_TIMEOUT = 0.1 * 60000, DOS_MAX_REQUEST = 100;
+// DoS
+const DOS_BLOCK_TIMEOUT = 5 * 60000, DOS_PROTECTION_TIMEOUT = 0.5 * 60000, DOS_PLUS_TIMEOUT = 2 * 60000, DOS_MAX_REQUEST = 100;
 let dosProtection = {};
+
+// DDoS
+const DDOS_MAX_REQUEST_COUNT = 100;
+let requestCount = 0, ddosQueue = [];
 
 start();
 
 function start() {
+
     files.push(fs.readFileSync(__dirname + "/public/index.html"));
     files.push(fs.readFileSync(__dirname + "/public/styles.css"));
     files.push(fs.readFileSync(__dirname + "/public/script.js"));
 
     server.listen(PORT);
     console.log(`Server listening on port ${PORT}`);
+
+}
+
+function ddosProtection(req, res) {
+    
+    if (requestCount++ < DDOS_MAX_REQUEST_COUNT) {
+        router(req, res);
+    } else {
+        ddosQueue.push({req, res});
+    }
+
 }
 
 function router(req, res) {
-    console.log(dosProtection);
 
     if (dosProtection[req.connection.remoteAddress] === undefined)
-        dosProtection[req.connection.remoteAddress] = {reqCount: 0, blocked: false, timeOfBlock: -1, blockTimeout: 0, numOfBlocks: 0};
+        dosProtection[req.connection.remoteAddress] = {
+            reqCount: 0,
+            blocked: false,
+            timeOfBlock: -1,
+            blockTimeout: 0,
+            numOfBlocks: 0};
+    
+    const currentDosProtection = dosProtection[req.connection.remoteAddress];
+    currentDosProtection.reqCount = currentDosProtection.reqCount + 1 || 1;
+    
+    if (currentDosProtection.reqCount > DOS_MAX_REQUEST) {
+        
+        if (currentDosProtection.blocked) {
 
-    if (dosProtection[req.connection.remoteAddress].reqCount > DOS_MAX_REQUEST) {
-        if (dosProtection[req.connection.remoteAddress].blocked) {
-            res.end(`Too many requests were sent from this IP address recently, so it has been blocked for ${dosProtection[req.connection.remoteAddress].timeOfBlock} minutes`);
+            if (Date.now() - currentDosProtection.timeOfBlock > currentDosProtection.blockTimeout) {
+                
+                currentDosProtection.blocked = false;
+                currentDosProtection.timeOfBlock = -1;
+                currentDosProtection.blockTimeout = 0;
+                currentDosProtection.reqCount = 0;
 
-            if (Date.now() - dosProtection[req.connection.remoteAddress].timeOfBlock > dosProtection[req.connection.remoteAddress].blockTimeout) {
-                dosProtection[req.connection.remoteAddress].blocked = false;
-                dosProtection[req.connection.remoteAddress].timeOfBlock = -1;
-                dosProtection[req.connection.remoteAddress].blockTimeout = 0;
-                dosProtection[req.connection.remoteAddress].reqCount = 0;
             } else
-                dosProtection[req.connection.remoteAddress].blockTimeout += DOS_PLUS_TIMEOUT * dosProtection[req.connection.remoteAddress].numOfBlocks;
+                currentDosProtection.blockTimeout += DOS_PLUS_TIMEOUT * currentDosProtection.numOfBlocks * (currentDosProtection.reqCount - DOS_MAX_REQUEST);
+            
+            if (currentDosProtection.blocked) {
+                handleNextRequest();
+                res.end(`DoS Védelem: Túl sok kérés jött erről az IP címről az utóbbi időben, úgyhogy tiltva lett ${currentDosProtection.blockTimeout / 60000} percre. Kérlek ne próbáld meg befrissíteni az oldalt, mert azzal csak plusz tiltási időt kapsz.`);
+            }
+
         } else {
-            dosProtection[req.connection.remoteAddress].blocked = true;
-            dosProtection[req.connection.remoteAddress].timeOfBlock = Date.now();
-            dosProtection[req.connection.remoteAddress].blockTimeout = DOS_BLOCK_TIMEOUT * ++dosProtection[req.connection.remoteAddress].numOfBlocks;
-            console.log(`DoS Block Cleared for ${req.connection.remoteAddress}`);
+
+            currentDosProtection.blocked = true;
+            currentDosProtection.timeOfBlock = Date.now();
+            currentDosProtection.blockTimeout = DOS_BLOCK_TIMEOUT * ++currentDosProtection.numOfBlocks;
+       
         }
-        if (!dosProtection[req.connection.remoteAddress].blocked)
+        
+        if (currentDosProtection.blocked)
             return;
+
     } else {
-        dosProtection[req.connection.remoteAddress].reqCount = dosProtection[req.connection.remoteAddress].reqCount + 1 || 1;
+
         setTimeout(() => {
-            if (!dosProtection[req.connection.remoteAddress].blocked)
-               dosProtection[req.connection.remoteAddress].reqCount--;
+            if (!currentDosProtection.blocked)
+                currentDosProtection.reqCount--;
         }, DOS_PROTECTION_TIMEOUT);
+    
     }
 
     if (req.method === "GET") {
+
         switch (req.url) {
+
             case "/":
                 res.writeHead(200, {"Content-Type": "text/html"});
                 res.end(files[0]);
@@ -75,9 +113,28 @@ function router(req, res) {
                 res.writeHead(404, {"Content-Type": "text/html"});
                 res.end("404: Page Not Round");
                 break;
+
         }
+
+        handleNextRequest();
+
     } else {
+
+        handleNextRequest();
         res.writeHead(405, {"Content-Type": "text/html"});
-        res.end(`405: Inappropriate request method (${req.method})`);
+        res.end(`405: Nem megfelelő HTTP metódus (${req.method})`);
+
     }
+}
+
+function handleNextRequest() {
+    
+    requestCount--;
+    
+    if (ddosQueue.length === 0)
+        return;
+
+    const nextReq = ddosQueue.shift();
+    router(nextReq.req, nextReq.res);
+
 }
