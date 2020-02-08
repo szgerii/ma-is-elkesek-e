dc74b4942df8d2617426e0a39e50eaf8da97be7e
rav <szucsi1104@gmail.com>
2020-02-08 17:07:23 +0100

    Add proper back-end logging
    Start in verbose mode by passing -v or --verbose to the program when starting Node. This will log basic things (server start, db connection, etc) and short descriptions of errors. For in-depth error logging use:
    
    Extra-verbose mode. Pass -xv or --extra-verbose to Node. This will log every received request, sent response and db refresh. It also prints the whole error object if something goes wrong.
    
    For outputting to a log file use -o [directory name]. The logger will create a 'server-[timestamp].log' file in the specified directory every time the server is started. Please don't try to access this file while the server is running, because the logger keeps a write stream open during execution. The file logging is set to extra-verbose mode by default (this can't be changed for now).

---
+++ src/private_modules/logger.js
@@ -0,0 +1,111 @@
+const fs = require('fs');
+
+const options = {};
+let fileOutputStream, fileOutputReady = false;
+
+function getCurrentTime(space) {
+	const d = new Date();
+	let day = d.getDate() < 10 ? `0${d.getDate()}` : d.getDate();
+	let month = d.getMonth() + 1 < 10 ? `0${d.getMonth() + 1}` : d.getMonth() + 1;
+	let hours = d.getHours() < 10 ? `0${d.getHours()}` : d.getHours();
+	let minutes = d.getMinutes() < 10 ? `0${d.getMinutes()}` : d.getMinutes();
+	let seconds = d.getSeconds() < 10 ? `0${d.getSeconds()}` : d.getSeconds();
+	if (space)
+		return `${d.getFullYear()}.${month}.${day}. ${hours}:${minutes}:${seconds}`;
+	else
+		return `${d.getFullYear()}.${month}.${day}_${hours}:${minutes}:${seconds}`;
+}
+
+exports.set = (key, value) => {
+	options[key] = value;
+
+	if (key === "file-output" && value === true) {
+		if (!options["file-output-dir"] || !options["file-output-name"]) {
+			console.log(options);
+			exports.force("LOGGER: File output path or file output name wasn't specified. File output has been turned off.");
+			options["file-output"] = false;
+			options["file-output-dir"] = null;
+			options["file-output-name"] = null;
+			return;
+		}
+		
+		fileOutputStream = fs.createWriteStream(`${options["file-output-dir"]}/${options["file-output-name"]}-${getCurrentTime()}.log`);
+		
+		fileOutputStream.on("error", (err) => {
+			fileOutputStream.close();
+			exports.force(`LOGGER: An error occured while accessing the log file. File output has been turned off. Turn on verbose mode for more information.`);
+			exports.log(`LOGGER: "${err}"`);
+			options["file-output"] = false;
+			options["file-output-dir"] = null;
+			options["file-output-name"] = null;
+		});
+
+		fileOutputStream.on("open", () => {
+			exports.write(`LOGGER: Writing to log file at '${fileOutputStream.path}'`);
+			fileOutputReady = true;
+		});
+	}
+};
+
+exports.get = key => {
+	return options[key];
+};
+
+exports.force = (message) => {
+	const msg = `[${getCurrentTime(true)}] ${message}`;
+	console.log(msg);
+	if (options["file-output"] && fileOutputReady)
+		fileOutputStream.write(msg + "\n");
+};
+
+exports.write = (message) => {
+	const msg = `[${getCurrentTime(true)}] ${message}`;
+	if (!options["silent"])
+		console.log(msg);
+	if (options["file-output"] && fileOutputReady)
+		fileOutputStream.write(msg + "\n");
+};
+
+exports.log = (message) => {
+	const msg = `[${getCurrentTime(true)}] ${message}`;
+	if (options["verbose"] || options["x-verbose"])
+		console.log(msg);
+	if (options["file-output"] && fileOutputReady)
+		fileOutputStream.write(msg + "\n");
+};
+
+exports.xLog = (message) => {
+	const msg = `[${getCurrentTime(true)}] ${message}`;
+	if (options["x-verbose"])
+		console.log(msg);
+	if (options["file-output"] && fileOutputReady)
+		fileOutputStream.write(msg + "\n");
+};
+
+exports.error = (message) => {
+	const msg = `[${getCurrentTime(true)}] ${message}`;
+	console.error(msg);
+	if (options["file-output"] && fileOutputReady)
+		fileOutputStream.write(msg + "\n");
+};
+
+exports.dir = (obj, message) => {
+	if (message) {
+		console.log(`[${getCurrentTime(true)}]`);
+		console.dir(obj);
+	}
+	else {
+		console.log(`[${getCurrentTime(true)}] ${message}`);
+		console.dir(obj);
+	}
+};
+
+exports.close = () => {
+	if (fileOutputReady) {
+		fileOutputStream.end();	
+		fileOutputReady = false;
+		options["file-output"] = false;
+		options["file-output-dir"] = null;
+		options["file-output-name"] = null;
+	}
+};
--- src/server.js
+++ src/server.js
@@ -1,86 +1,142 @@
-const   http     = require('http'),
-		fs       = require('fs'),
-		mongoose = require('mongoose'),
-		section  = require('./models/section');
+// Node modules
+const http = require('http');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const logger = require('./private_modules/logger');
 
+// Mongoose models
+const section = require('./models/section');
+
+// Constants
 const PORT = 1104 || process.env.PORT;
 const DB_URL = "mongodb+srv://node-server:9ahtrXRyp5sRvm77@ma-is-elkesek-e-ejiov.mongodb.net/ma-is-elkesek-e?retryWrites=true&w=majority";
+const DB_REFRESH_INTERVAL = 30000; // milliseconds
+
+// Other variables
 const server = http.createServer(router);
 const files = [];
-const top3 = {};
+const top3 = {}; // Hot smokin top 3 sections
+let lastDBCheck; // Last hot smokin top 3 pull from DB
 
-let lastDBCheck;
+setup();
 
-mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-mongoose.connection.on("connected", () => {
-	console.log(`Successfully connected to MongoDB on ${DB_URL}`);
-});
+// Sets up everything needed for the server
+async function setup() {
+	// Logger setup
+	logger.set("verbose", process.argv.includes("-v") || process.argv.includes("--verbose"));
+	logger.set("x-verbose", process.argv.includes("-xv") || process.argv.includes("--extra-verbose"));
+	logger.set("silent", process.argv.includes("-s") || process.argv.includes("--silent"));
+	const outputArgIndex = process.argv.indexOf("-o");
+	if (outputArgIndex !== -1) {
+		logger.set("file-output-dir", process.argv[outputArgIndex + 1] || ".");
+		logger.set("file-output-name", "server");
+		logger.set("file-output", true);
+	}
 
-getHotSmokin();
-start();
+	// Ctrl+C
+	process.on('SIGINT', () => {
+		logger.log("Shutting down...");
+		logger.close();
+		process.exit(0);
+	});
 
-// Loads files into memory and starts listening on PORT
-function start() {
+	getHotSmokin().catch(err => {
+		logger.error("Couldn't get top 3 list from database");
+		logger.xLog(err);
+	});
+
+	// Load public files into files
 	files.push(fs.readFileSync(__dirname + "/public/index.html")); // 0
 	files.push(fs.readFileSync(__dirname + "/public/styles.css")); // 1
 	files.push(fs.readFileSync(__dirname + "/public/script.js")); // 2
-	files.push(fs.readFileSync(__dirname + "/public/lib/Jquery.js")); // 3
+	files.push(fs.readFileSync(__dirname + "/public/lib/jquery.js")); // 3
 	files.push(fs.readFileSync(__dirname + "/public/assets/images/background.png")); // 4
 	files.push(fs.readFileSync(__dirname + "/public/assets/images/icon.png")); // 5
 
+	// Mongoose setup
+	mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).catch(err => {
+		logger.error("Couldn't connect to the database. Shutting down...");
+		logger.xLog(err);
+		process.exit(1);
+	});
+	
+	mongoose.connection.on("connected", () => {
+		logger.log(`Successfully connected to the database`);
+	});
+	
+	mongoose.connection.on("error", err => {
+		logger.error("Database connection was terminated. Shutting down...");
+		logger.xLog(err);
+		process.exit(1);
+	});
+	
 	server.listen(PORT);
-	console.log(`Server listening on port ${PORT}`);
+	logger.log(`Server listening on port ${PORT}`);
 }
 
-async function router(req, res) {
-	await parseReq(req);
+// The request handler function
+function router(req, res) {
+	parseReq(req);
+
+	const ip = (req.headers['x-forwarded-for'] || '').split(',').pop() || 
+    	req.connection.remoteAddress || 
+    	req.socket.remoteAddress || 
+    	req.connection.socket.remoteAddress;
+	logger.xLog(`${req.method} request at ${req.url} from ${ip}`);
 	
-	// Check if the method is correct
 	if (req.method === "GET") {
-		// Send response to client
 		switch (req.baseUrl) {
 			case "/":
 				res.writeHead(200, {"Content-Type": "text/html"});
 				res.end(files[0]);
+				logger.xLog(`Sending ${res.statusCode} response to ${ip}`);
 				break;
 			
 			case "/styles.css":
 				res.writeHead(200, {"Content-Type": "text/css"});
 				res.end(files[1]);
+				logger.xLog(`Sending ${res.statusCode} response to ${ip}`);
 				break;
 			
 			case "/script.js":
 				res.writeHead(200, {"Content-Type": "text/javascript"});
 				res.end(files[2]);
+				logger.xLog(`Sending ${res.statusCode} response to ${ip}`);
 				break;
 
-			case "/Jquery.js":
+			case "/jquery.js":
 				res.writeHead(200, {"Content-Type": "text/javascript"});
 				res.end(files[3]);
+				logger.xLog(`Sending ${res.statusCode} response to ${ip}`);
 				break;
 
 			case "/assets/images/background.png":
 				res.writeHead(200, {"Content-Type": "image/png"});
 				res.end(files[4]);
+				logger.xLog(`Sending ${res.statusCode} response to ${ip}`);
 				break;
 
 			case "/assets/images/icon.png":
 				res.writeHead(200, {"Content-Type": "image/png"});
 				res.end(files[5]);
+				logger.xLog(`Sending ${res.statusCode} response to ${ip}`);
 				break;
 			
 			case "/hotsmokin":
 				getHotSmokin().then(top3 => {
 					res.writeHead(200, {"Content-Type": "application/json"});
 					res.end(JSON.stringify(top3));
+					logger.xLog(`Sending ${res.statusCode} response to ${ip}`);
 				}).catch(err => {
-					console.error(err);
+					logger.error("Couldn't get top 3 list from database");
+					logger.xLog(err);
 				});
 				break;
 
 			default:
 				res.writeHead(404, {"Content-Type": "text/html"});
 				res.end("404: Page Not Round");
+				logger.xLog(`Sending ${res.statusCode} response to ${ip}`);
 				break;
 		}
 	} else if (req.method === "POST") {
@@ -91,26 +147,32 @@ async function router(req, res) {
 		});
 	
 		req.on("end", async () => {
-			req.body = await queryParser(decodeURIComponent(data.concat().toString()), false);
-			
+			req.body = queryParser(decodeURIComponent(data.concat().toString()), false);
+
 			switch (req.baseUrl) {
 				case "/hotsmokin":
 					const b = req.body;
-					await updateSection(b.lineId, b.lineName, b.stop1Id, b.stop1Name, b.stop2Id, b.stop2Name);
+					updateSection(b.lineId, b.lineName, b.stop1Id, b.stop1Name, b.stop2Id, b.stop2Name);
 					break;
 			
 				default:
 					res.writeHead(404, {"Content-Type": "text/html"});
 					res.end("404: Page Not Round");
+					logger.xLog(`Sending ${res.statusCode} response to ${ip}`);
 					break;
 			}
 		});
 	} else {
 		res.writeHead(405, {"Content-Type": "text/html"});
 		res.end(`405: Érvénytelen HTTP metódus (${req.method})`);
+		logger.xLog(`Sending ${res.statusCode} response to ${ip}`);
 	}
 }
 
+/*
+	Fills up the request object with the base url (without queries) and a separate query object
+	req: request object
+*/
 function parseReq(req) {
 	const urlParts = req.url.split('?');
 	req.baseUrl = urlParts[0];
@@ -121,34 +183,50 @@ function parseReq(req) {
 	req.query = queryParser(urlParts[1]);
 }
 
+/*
+	Parses a query string into a query object
+	rawQuery: the unparsed query string
+	detectType: if true, the function converts the query values, if possible
+*/
 function queryParser(rawQuery, detectType) {
 	const query = {};
 
 	rawQuery.split('&').forEach(data => {
 		const splitData = data.split('=');
 		
-		if (!detectType) {
+		if (!detectType)
 			query[splitData[0]] = splitData[1];
-		} else if (splitData[1] === "true") {
+		else if (splitData[1] === "true")
 			query[splitData[0]] = true;
-		} else if (splitData[1] === "false") {
+		else if (splitData[1] === "false")
 			query[splitData[0]] = false;
-		} else if (!Number.isNaN(Number(splitData[1]))) {
+		else if (!Number.isNaN(Number(splitData[1])))
 			query[splitData[0]] = Number(splitData[1]);
-		} else {
+		else
 			query[splitData[0]] = splitData[1];
-		}
 	});
 
 	return query;
 }
 
+/*
+	Looks up a section in the database and
+		a) increments its count by one if it founds it
+		b) creates it if it doesn't exist yet
+	lineId: id of the line (lookup value)
+	lineName: name of the line
+	stop1Id: id of the first stop (lookup value)
+	stop1Name: name of the first stop
+	stop2Id: id of the second stop (lookup value)
+	stop2Name: name of the second stop
+*/
 async function updateSection(lineId, lineName, stop1Id, stop1Name, stop2Id, stop2Name) {
 	let sec = await section.findOne({ lineId: lineId, stop1Id: stop1Id, stop2Id: stop2Id });
 	
 	if (sec) {
 		sec.count++;
 		await sec.save();
+		logger.xLog(`Increased the following section's count in the database -> ${sec.lineName}: ${sec.stop1Name} - ${sec.stop2Name}`);
 	} else {
 		sec = new section();
 		sec.lineId = lineId;
@@ -159,25 +237,39 @@ async function updateSection(lineId, lineName, stop1Id, stop1Name, stop2Id, stop
 		sec.stop2Name = stop2Name.replace(/\+/g, " ");
 		sec.count = 1;
 		await sec.save();
+		logger.xLog(`Adding the following section to the database -> ${sec.lineName}: ${sec.stop1Name} - ${sec.stop2Name}`);
 	}
 }
 
+/*
+	Returns a promise that checks if the top 3 list should be updated and
+		a) calls the update function if necessary, and resolves with the top 3 list
+		b) resolves the top 3 list if update isn't necessary
+		c) rejects with the error if something went wrong
+*/
 function getHotSmokin() {
 	return new Promise((resolve, reject) => {
-		if (!lastDBCheck || Date.now() - lastDBCheck > 5000 || !top3.hot1 || !top3.hot2 || !top3.hot3) {
+		if (!lastDBCheck || Date.now() - lastDBCheck > DB_REFRESH_INTERVAL || !top3.hot1 || !top3.hot2 || !top3.hot3) {
 			updateHotSmokin().then(() => {
+				logger.xLog("Successfully refreshed top 3 section from database");
 				resolve(top3);
 			}).catch(err => {
 				reject(err);
 			});
-
+			
 			lastDBCheck = Date.now();
 		} else {
+			logger.xLog(`Responding with cached top 3 list, ${DB_REFRESH_INTERVAL - (Date.now() - lastDBCheck)} ms before next update`);
 			resolve(top3);
 		}
 	});
 }
 
+/*
+	Returns a promise that updates the top 3 hot smokin section from the database and
+		a) resolves if the update was successful
+		b) rejects with the error if something went wrong
+*/
 function updateHotSmokin() {
 	return new Promise((resolve, reject) => {
 		top3.hot1 = null;
