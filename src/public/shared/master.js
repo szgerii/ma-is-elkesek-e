1498c73db8920f505da20f21a09427b3d4262a66
rav <szucsi1104@gmail.com>
2020-04-13 01:04:25 +0200

    Add cookie consent footer
    The website now warns the user about using cookies.

--- src/public/shared/master.js
+++ src/public/shared/master.js
@@ -1,10 +1,17 @@
+const getCookie = cookie => {
+	let cookies = document.cookie.split("; ");
+
+	for (let i = 0; i < cookies.length; i++) {
+		if (cookies[i].split("=")[0] === cookie)
+			return cookies[i].split("=")[1];
+	}
+
+	return null;
+};
+
 const getUsername = () => {
-
-    let cookies = document.cookie.split("; ");
-
-    for (let i=0; i<cookies.length; i++) if (cookies[i].split("=")[0]=="username") return cookies[i].split("=")[1];
-
-}
+	return getCookie("username");
+};
 
 function setup() {
 	const navbarMenu = document.querySelector(".navbar-menu");
@@ -22,6 +29,28 @@ function setup() {
 				window.location.assign("/");
 			});
 		});
+	}
+
+	if (!getCookie("cookie-consent")) {
+		document.documentElement.classList.add("extended");
+		document.body.classList.add("extended");
+		const footer = document.createElement("footer");
+		const p = document.createElement("p");
+		const button = document.createElement("button");
+
+		p.innerText = "Ez az oldal sütiket használ a működéshez. Az oldal használatával automatikusan hozzájárul a sütik használatához.";
+		
+		button.innerText = "Elfogadás";
+		button.addEventListener("click", () => {
+			document.cookie = "cookie-consent=true; expires=Sun, 09 Jun 2069 00:00:00 GMT;";
+			document.querySelector("footer").remove();
+			document.body.classList.remove("extended");
+			document.documentElement.classList.remove("extended");
+		});
+		
+		footer.appendChild(p);
+		footer.appendChild(button);
+		document.body.appendChild(footer);
 	}
 }
 
