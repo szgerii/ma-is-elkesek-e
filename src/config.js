cac44bd91e2e9ea9632bc10e94786959fb24847e
rav <szucsi1104@gmail.com>
2020-04-13 21:50:56 +0200

    Remove config.js values

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
+const dbUsername = "<USERNAME COMES HERE>";
+const dbPassword = "<PASSWORD COMES HERE>";
+
 module.exports = {
-	databaseUrl: "mongodb+srv://dev:XIjiwpN7DlQgNvVq@ma-is-elkesek-e-ejiov.mongodb.net/dev?retryWrites=true&w=majority",
-	jwtKey: "ee2c1a1df5ca089a92993b24831dd994",
+	databaseUrl: `mongodb+srv://${dbUsername}:${dbPassword}@ma-is-elkesek-e-ejiov.mongodb.net/live?retryWrites=true&w=majority`,
+	jwtKey: "<JWT KEY COMES HERE>",
 	authTokenMaxAge: 30 * 24 * 60 * 60,
-	domain: "" // TODO: replace with domain name after hosting has been set up
+	domain: "ma-is-elkesek-e.hu"
 };
