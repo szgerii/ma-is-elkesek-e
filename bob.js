5f2d14ba6defc775a0ba292a3169f8a101ea3b12
hentesoposszum <szucsi1104@gmail.com>
2020-04-17 20:49:57 +0200

    Add compression to Bob
    The build tool can now compress files to gzip with zlib.

--- bob.js
+++ bob.js
@@ -2,50 +2,62 @@
 
 const fs = require("fs");
 const path = require("path");
+const { EOL } = require("os");
 
-const minify = require("minify");
-const terser = require("terser");
+const minify = require("minify"); // HTML and CSS minification
+const terser = require("terser"); // JS minification
+const zlib = require("zlib"); // gzip compression
 
 const logger = require("./private_modules/logger");
 logger.set("x-verbose", true);
 
-const buildConfigDir = process.env.BUILD_CONFIG || path.join(__dirname, ".build_config");
 const deleteProdDir = process.argv.includes("--delete-prod-dir") || process.argv.includes("-dp");
 const ignoreErrors = process.argv.includes("--ignore-errors") || process.argv.includes("-ie");
 const stopOnWarning = process.argv.includes("--stop-on-warning") || process.argv.includes("-sw");
 
-if (!fs.existsSync(path.join(buildConfigDir, "minify.json"))) {
-    logger.error("minify.json file missing. Shutting down...");
-    logger.close();
-    process.exit(1);
-}
+const buildConfigDir = process.env.BUILD_CONFIG || path.join(__dirname, ".build_config");
+const minifyPath = process.argv.includes("--min") ? path.resolve(process.argv[process.argv.indexOf("--min") + 1]) : path.join(buildConfigDir, "minify.json");
+const compressPath = process.argv.includes("--comp") ? path.resolve(process.argv[process.argv.indexOf("--comp") + 1]) : path.join(buildConfigDir, "compress.json");
+
+const prodBase = process.argv.includes("--prod-dir") ? path.resolve(process.argv[process.argv.indexOf("--prod-dir") + 1]) : path.resolve("dist");
+const devBase = process.argv.includes("--dev-dir") ? path.resolve(process.argv[process.argv.indexOf("--dev-dir") + 1]) : path.resolve("src");
 
 build();
 
 async function build() {
-    logger.log("--BOB-- Starting the build process...");
+    if (deleteProdDir) {
+        fs.rmdirSync(path.resolve(prodBase), {
+            recursive: true
+        });
+    }
+
+    logger.log("--BOB-- Starting the build process..." + EOL);
     
-    logger.log("--BOB-- Minifying the files...");
+    logger.log("--BOB-- Minifying the files..." + EOL);
     await minifyFiles();
-    logger.log("--BOB-- Minification done.");
+    logger.log("--BOB-- Minification done." + EOL);
     
-    logger.log("--BOB-- Build done.");
+    logger.log("--BOB-- Compressing the files..." + EOL);
+    await compressFiles();
+    logger.log("--BOB-- Compression done." + EOL);
+    
+    logger.log("--BOB-- Build done." + EOL);
 }
 
 
 async function minifyFiles() {
-    const files = JSON.parse(fs.readFileSync(path.join(buildConfigDir, "minify.json")));
-    const devBase = path.resolve(files.devBase), prodBase = path.resolve(files.prodBase);
- 
-    if (deleteProdDir)
-        fs.rmdirSync(path.resolve(prodBase), {
-            recursive: true
-        });
+    if (!fs.existsSync(minifyPath)) {
+        logger.error("Minification config file not found. Shutting down...");
+        logger.close();
+        process.exit(1);
+    }
+
+    const files = JSON.parse(fs.readFileSync(minifyPath));
 
     if (!fs.existsSync(path.resolve(prodBase)))
         fs.mkdirSync(path.resolve(prodBase));
     
-    for (const f of files.fileList) {
+    for (const f of files) {
         let dir = path.join(prodBase, f);
         dir = dir.slice(0, dir.lastIndexOf(path.sep));
 
@@ -66,18 +78,23 @@ async function minifyFiles() {
                 logger.dir(minified.error);
 
                 if (!ignoreErrors) {
-                    logger.log(`Shutting down...`);
+                    logger.log(`Build failed. Shutting down...${EOL}`);
                     logger.close();
                     process.exit(2);
                 }
             }
 
             if (minified.warnings) {
-                logger.log(`Warning from the following file: ${f}`);
-                logger.log(minified.warnings);
+                logger.log(`Warning(s) from the following file: ${f}`);
+
+                for (const warning of minified.warnings) {
+                    logger.log(warning);
+                }
+
+                console.log();
 
                 if (stopOnWarning) {
-                    logger.log(`Shutting down...`);
+                    logger.log(`Build failed. Shutting down...`);
                     logger.close();
                     process.exit(3);
                 }
@@ -85,5 +102,24 @@ async function minifyFiles() {
 
             fs.writeFileSync(path.join(prodBase, f), minified.code);
         }
+    }
+}
+
+async function compressFiles() {
+    if (!fs.existsSync(compressPath)) {
+        logger.error("Minification config file not found. Shutting down...");
+        logger.close();
+        process.exit(1);
+    }
+
+    const files = JSON.parse(fs.readFileSync(compressPath));
+
+    for (const f of files) {
+        let dir = path.join(prodBase, f);
+        dir = dir.slice(0, dir.lastIndexOf(path.sep));
+
+        fs.mkdirSync(dir, {recursive: true});
+
+        fs.writeFileSync(path.join(prodBase, f), zlib.deflateSync(fs.readFileSync(path.join(devBase, f))));
     }
 }
