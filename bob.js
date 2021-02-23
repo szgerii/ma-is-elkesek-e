ff5422b9358292f0472cf410b66e5a0e13f4d0f3
rav <szucsi1104@gmail.com>
2021-02-23 10:17:29 +0100

    Fix the previous fix
    It happens.

--- bob.js
+++ bob.js
@@ -372,7 +372,7 @@ function makeUnique() {
     const excludeRegExp = new RegExp(uniqueExclude);
 
     for (let i = 0; i < fileList.length; i++) {
-        if (excludeRegExp.test(fileList[i]) || fileList[i].includes("files.json"))
+        if (excludeRegExp.test(fileList[i]) || fileList[i].includes("files.json") || fileList[i].includes("robots.txt"))
             continue;
 
         const oldFileNameRegExp = new RegExp(fileList[i].split("/").slice(-1)[0], "g");
